834ac70433d3176258fac680577eb0ca490cb2bd
Dern965 <dern965@hotmail.com>
2024-12-18 17:47:19 -0600

    Extractor listado de profesores listo

---
+++ Proyecto/Extractor_comentarios.py
@@ -0,0 +1,21 @@
+from selenium.webdriver import Chrome
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+
+def obtener_links(driver):
+    pass
+
+def obtener_comentarios(driver, links):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
